b9ceb993d169416d26196b63a968baf9aa9fb878
Conor Byrne (Home) <conorb@gmail.com>
2013-10-20 23:16:41 +0100

    Working on getting grid to line up.
    Not fucking there yet!

--- marblegame/src/com/barconr/games/marblegame/Assets.java
+++ marblegame/src/com/barconr/games/marblegame/Assets.java
@@ -10,10 +10,17 @@ public class Assets{
 	
 	public static final int VIRTUAL_SCREEN_HEIGHT = 480;
 	public static final int VIRTUAL_SCREEN_WIDTH = 800;
+	
+//	public static final float VIRTUAL_SCREEN_HEIGHT = 160;
+//	public static final float VIRTUAL_SCREEN_WIDTH = 266.666666667f;
+	
+	
 	public static TiledMap mazemap;
 	
 	public static final float HEIGHT_METERS = 6;
 	public static final float WIDTH_METERS = 10;
+//	public static final float HEIGHT_METERS = 18;
+//	public static final float WIDTH_METERS = 30;
 	
 	
 	public static final float ASPECT_RATIO= (float) VIRTUAL_SCREEN_WIDTH / VIRTUAL_SCREEN_HEIGHT;
@@ -29,7 +36,8 @@ public class Assets{
 	public static void load(){
 		gameAtlas = new TextureAtlas(Gdx.files.internal("gfx/marble.pack"));
 		marble = new TextureRegion(gameAtlas.findRegion("marb"));
-		mazemap = new TmxMapLoader().load("tiles/test map.tmx");
+		mazemap = new TmxMapLoader().load("tiles/mg.tmx");
+		//new TmxMapLoader().lo
 		
 	}
 	
--- marblegame/src/com/barconr/games/marblegame/MarbleGame.java
+++ marblegame/src/com/barconr/games/marblegame/MarbleGame.java
@@ -1,5 +1,8 @@
 package com.barconr.games.marblegame;
 
+import java.util.Iterator;
+
+import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
@@ -9,7 +12,15 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTile;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
@@ -50,14 +61,20 @@ public class MarbleGame implements ApplicationListener {
 		camera.position.set(Assets.VIRTUAL_SCREEN_WIDTH/2f,Assets.VIRTUAL_SCREEN_HEIGHT/2f, 0);
 		camera.update();
 		
+
+		
 		tilerender = new OrthogonalTiledMapRenderer(Assets.mazemap);
+		//createBoxShit();
+		createFixturesFromMapII(Assets.mazemap);
+		
 		debugProj = new Matrix4(camera.combined);
 		debugProj.scale(Assets.PIXELS_PER_METER, Assets.PIXELS_PER_METER, 1);
 		batcher = new SpriteBatch();
 		batcher.setProjectionMatrix(camera.combined);
 		batcher.enableBlending();
-		createBoxShit();
+		
 		tilerender.setView(camera);
+		
 		debugRenderer = new Box2DDebugRenderer();
 		
 	}
@@ -67,10 +84,12 @@ public class MarbleGame implements ApplicationListener {
 	
 	public void createBoxShit(){
 		world = new World(new Vector2(0, -9), true); 
+		
 		//world.
         BodyDef groundBodyDef =new BodyDef();  
-        groundBodyDef.position.set(new Vector2(5, 0.3f));  
+        groundBodyDef.position.set(new Vector2(5, 1f));  
         Body groundBody = world.createBody(groundBodyDef);  
+        groundBodyDef.type = BodyType.StaticBody;
         PolygonShape groundBox = new PolygonShape();  
         //groundBox.setAsBox(20, 0.3f);  
         groundBox.setAsBox(5f, 0.3f);
@@ -102,11 +121,83 @@ public class MarbleGame implements ApplicationListener {
         circleBody.createFixture(fixtureDef);  
         
         
+        PolygonShape squareShape = new PolygonShape();
+        BodyDef squareBodyDef = new BodyDef();
+        squareBodyDef.position.sub(1* Assets.METERS_PER_PIXEL, 1* Assets.METERS_PER_PIXEL);
+        
+        squareShape.setAsBox(50 * Assets.METERS_PER_PIXEL, 50 * Assets.METERS_PER_PIXEL);
+        FixtureDef fixDefSquare = new FixtureDef();
+        fixDefSquare.shape = squareShape;
+        fixDefSquare.density = 0.1f;
+        fixDefSquare.restitution= 0.3f;
+        Body squareBody = world.createBody(squareBodyDef);
+        squareBody.createFixture(fixDefSquare);
         
         
         
         
         
+        
+	}
+	
+	
+	
+	void createFixturesFromMapII(TiledMap tiledmap){
+		int layerHeight =0 , layerWidth=0;
+		float tileWidth=0 ,tileHeight=0 ;
+		
+		TiledMapTileLayer tilelayer = (TiledMapTileLayer)(tiledmap.getLayers().get(0));
+		layerHeight = tilelayer.getHeight();
+		layerWidth = tilelayer.getWidth();
+		tileHeight = tilelayer.getTileHeight();
+		tileWidth = tilelayer.getTileWidth();
+		
+		System.out.println("Layer height (tiles) = " + layerHeight+" Layer width (tiles) = " + layerWidth+ " Tile height = " + tileHeight+"tile width = " + tileWidth);
+		System.out.println("Tile count = "+ tilelayer.getObjects().getCount());
+		System.out.println("layer height pixels = " + (layerHeight*tileHeight) + "LAYER WIDTH PIXELS : " + (layerWidth*tileWidth) );
+		
+		//Loop through tilemap
+		
+		//Create the box2d world
+		world = new World(new Vector2(0, -9), true); 
+		
+		
+		
+		
+		
+		for(int i=0;i<layerHeight;i++){
+			
+			for(int j=0;j<layerWidth;j++){
+				boolean impassibleBlock = tilelayer.getCell(j, layerHeight-i-1).getTile().getProperties().containsKey("block");
+				if(impassibleBlock){
+					//Draw box for fixture that is impassible
+					PolygonShape squareShape = new PolygonShape();
+			        BodyDef squareBodyDef = new BodyDef();
+			        squareBodyDef.type = BodyType.StaticBody;
+			        //Box position
+			        squareBodyDef.position.set( new Vector2(j* Assets.METERS_PER_PIXEL*16, (layerHeight-i-1)* Assets.METERS_PER_PIXEL*16));
+			        Body squareBody = world.createBody(squareBodyDef);
+			        //Size of box
+			        squareShape.setAsBox(16 * Assets.METERS_PER_PIXEL, 16 * Assets.METERS_PER_PIXEL);
+			        FixtureDef fixDefSquare = new FixtureDef();
+			        fixDefSquare.shape = squareShape;
+			        fixDefSquare.density = 0.1f;
+			        fixDefSquare.restitution= 0.3f;
+			        squareBody.createFixture(fixDefSquare);
+					
+					System.out.print("@");
+					
+				}
+				else{
+					System.out.print("#");
+				}
+				
+			}
+			System.out.println();
+		}
+		
+		
+		
 	}
 	
 	
@@ -126,7 +217,8 @@ public class MarbleGame implements ApplicationListener {
 //		batcher.draw(Assets.marble,circleBody.getPosition().x,circleBody.getPosition().y * Assets.PIXELS_PER_METER);
 //		batcher.end();
 //		
-		 
+		
+		
 		tilerender.render();
 		debugRenderer.render(world, debugProj);
 		//System.out.println(bodyDef.position.x*Assets.PIXELS_PER_METER +""+bodyDef.position.y * Assets.PIXELS_PER_METER);
